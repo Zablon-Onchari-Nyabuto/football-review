8a063dcd36357cd74227653fa276742d7a575884
Zablon-Onchari-Nyabuto <zablon.nyabuto@student.moringaschool.com>
2022-12-06 14:52:36 +0300

    create homepage, forum, contact us, about us components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Homepage from './components/Homepage';
-// import Forum from './components/Forum';
-// import ContactUs from "./components/ContactUs"
-// import AboutUs from './components/AboutUs';
+import Homepage from './components/Homepage';
+
 import NavBar from "./components/Navbar";
-// import { Route, Routes } from "react-router-dom";
+import Forum from './components/Forum';
+import AboutUs from './components/AboutUs';
+import ContactUs from './components/ContactUs';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
 
   return (
+    <BrowserRouter>
     <div className='App'>
       <NavBar />
-      {/* <Routes>
+      <Routes>
       <Route path={'/'}  element={<Homepage />} />
         <Route path={"/Forum"}  element={<Forum />} />
         <Route path={"/aboutus"} element={<AboutUs />}/>
         <Route path={"/contactus"} element={<ContactUs />} />
-      </Routes> */}
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
