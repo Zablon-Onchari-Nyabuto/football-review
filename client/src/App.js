056a9655e9659ffa850c411eb254398ae6daf9d6
Zablon-Onchari-Nyabuto <zablon.nyabuto@student.moringaschool.com>
2022-12-07 16:10:16 +0300

    Add login feature and authentication code

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Homepage from './components/Homepage';
-
+import Login from "./pages/Login";
 import NavBar from "./components/Navbar";
 import Forum from './components/Forum';
 import AboutUs from './components/AboutUs';
@@ -13,18 +13,30 @@ import {
 } from "react-router-dom";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (!user) return <Login onLogin={setUser} />;
 
   return (
     <BrowserRouter>
-    <div className='App'>
-      <NavBar />
-      <Routes>
-      <Route path={'/'}  element={<Homepage />} />
-        <Route path={"/Forum"}  element={<Forum />} />
-        <Route path={"/aboutus"} element={<AboutUs />}/>
-        <Route path={"/contactus"} element={<ContactUs />} />
-      </Routes>
-    </div>
+      <div className='App'>
+        <NavBar />
+        <Routes>
+          <Route path={'/'} element={<Homepage />} />
+          <Route path={"/Forum"} element={<Forum />} />
+          <Route path={"/aboutus"} element={<AboutUs />} />
+          <Route path={"/contactus"} element={<ContactUs />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
